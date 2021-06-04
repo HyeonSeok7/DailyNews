1c9f3a10dbbade3243682f109312dd36432ad90a
startwo09 <startwo09@gamil.com>
2021-06-04 17:52:28 +0900

    [Add] HomeFragment: 새로고침 추가

--- app/src/main/java/com/example/dailynews/feature/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/dailynews/feature/main/home/HomeFragment.kt
@@ -1,25 +1,37 @@
 package com.example.dailynews.feature.main.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
-import com.example.dailynews.MainActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.dailynews.R
+import com.example.dailynews.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        bindUI()
+        return binding.root
     }
 
+    override fun onRefresh() {
+        binding.layoutRefresh.isRefreshing = false
+    }
+
+    private fun bindUI() {
+        binding.layoutRefresh.setOnRefreshListener(this)
+    }
 
     companion object {
         private val TAG = HomeFragment::class.java.simpleName
